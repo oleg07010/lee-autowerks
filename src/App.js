f39aceaa2572614d544a1258d7410021e1b66073
Oleg <42892503+oleg07010@users.noreply.github.com>
2024-10-22 20:07:01 -0400

    Cleared git cache and re-added

--- src/App.js
+++ src/App.js
@@ -7,20 +7,19 @@ import AboutUs from './pages/AboutUs/AboutUs';
 import Footer from './components/Footer/Footer';
 import Divider from '@mui/material/Divider';
 // Add this import
-import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <Router>
       <div className='App'>
         <NavBar />
-        <Layout>
+        
           <Routes>
             <Route path='/' element={<Home/>} />
             <Route path='/services' element={<Services/>} />
             <Route path='/about-us' element={<AboutUs/>} />
           </Routes>
-        </Layout>
+       
         <Divider /> 
         <Footer/>
       </div>
